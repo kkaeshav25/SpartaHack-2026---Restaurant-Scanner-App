1617fffe5aa62b20601bc439e1769cd488670582
Kaeshav <kkaeshav@gmail.com>
2026-01-31 16:34:34 -0500

    Some frontend ideas

--- frontend.swift
+++ frontend.swift
@@ -1,2 +1,95 @@
 import SwiftUI
 import UIKit
+
+import SwiftUI
+import UIKit
+
+struct CameraView: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: CameraView
+
+        init(_ parent: CameraView) {
+            self.parent = parent
+        }
+
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+        ) {
+            parent.image = info[.originalImage] as? UIImage
+            picker.dismiss(animated: true)
+        }
+    }
+}
+func base64(_ image: UIImage) -> String? {
+    image.jpegData(compressionQuality: 0.7)?
+        .base64EncodedString()
+}
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    @Published var location: CLLocation?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        location = locations.first
+    }
+}
+func fetchMenu(
+    image: UIImage,
+    location: CLLocation,
+    completion: @escaping (String?) -> Void
+) {
+    guard let img64 = base64(image) else { return }
+
+    let body: [String: Any] = [
+        "image_base64": img64,
+        "latitude": location.coordinate.latitude,
+        "longitude": location.coordinate.longitude
+    ]
+
+    let url = URL(string: "https://YOUR_BACKEND_URL/menu")!
+    var req = URLRequest(url: url)
+    req.httpMethod = "POST"
+    req.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    req.httpBody = try? JSONSerialization.data(withJSONObject: body)
+
+    URLSession.shared.dataTask(with: req) { data, _, _ in
+        guard let data = data,
+              let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any]
+        else {
+            completion(nil)
+            return
+        }
+
+        completion(json["menu_url"] as? String)
+    }.resume()
+}
+if let url = menuURL {
+    Link("View Menu", destination: URL(string: url)!)
+}
